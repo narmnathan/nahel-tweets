73070e9b1645877844f60cbf8c2602805a3d8929
narmnathan <narmnathan@gmail.com>
2023-09-25 20:38:20 -0400

    added translated tweet csv, analysis guide, translate script

--- translate.py
+++ translate.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from deep_translator import GoogleTranslator
 import concurrent.futures
+import time
 
 translator = GoogleTranslator(source='auto', target='english')
 tweets = []
 
 def translate_tweet(str):
+    time.sleep(1)
     print('Translating: ' + str)
     translated = translator.translate(text=str)
     tweets.append(translated)
@@ -14,9 +16,12 @@ def main():
     df = pd.read_csv('tweet_text.csv')
     tweets = df['tweet_text']
     
-    with concurrent.futures.ThreadPoolExecutor(max_workers=7) as executor: 
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor: # setting to five workers since api request maxes out at 5
         executor.map(translate_tweet, tweets)
         
     df_translated = pd.DataFrame(tweets, columns=[['translated']])
     
     df_translated.to_csv('translated.csv', index=False)
+    
+if __name__ == "__main__":
+    main()
